877a7d64ae422c04ae3dd33089ddf9ebf1ada6f2
Andrew Smith <andy@robotlovesyou.com>
2025-09-19 14:30:55 +0200

    add wall sliding and wall jumping

--- player.gd
+++ player.gd
@@ -3,20 +3,18 @@ extends CharacterBody2D
 @export var movement_config: PlayerMovementConfig
 
 
-enum Action {NONE, IDLE, FALLING, WALKING, JUMPING, DOUBLE_JUMPING}
+enum Action {NONE, IDLE, FALLING, WALKING, JUMPING, DOUBLE_JUMPING, WALL_SLIDING_UP, WALL_SLIDING_DOWN, WALL_JUMPING}
 func _action_name(action: Action) -> String: return Action.find_key(int(action))
 
 signal began_action(Action)
 
-
-# const SPEED = 300.0
-# const JUMP_VELOCITY = -400.0
 
 var _action: Action = Action.NONE
 var _direction: float = 1.0
 var _last_direction: float = 1.0
 var _has_jumped: bool = false
 var _has_stopped_jump: bool = false
+var _wall_jump_normal: Vector2
 
 
 func _begin_action(action: Action) -> void:
@@ -27,7 +25,7 @@ func _begin_action(action: Action) -> void:
 
 func _is_jumping() -> bool:
 	# return true for any jumping action (jumping, double jumping, wall jumping)
-	return _action == Action.JUMPING or _action == Action.DOUBLE_JUMPING
+	return _action == Action.JUMPING or _action == Action.DOUBLE_JUMPING or _action == Action.WALL_JUMPING
 
 func _has_direction() -> bool:
 	return _direction != 0.0
@@ -52,13 +50,17 @@ func _physics_process(delta: float) -> void:
 
 func _determine_direction() -> void:
 	var temp_direciton = _direction
-	_direction = Input.get_axis("ui_left", "ui_right")
+	if $WallJumpTimer.time_left > 0.0:
+		_direction = _wall_jump_normal.x
+	else:
+		_direction = Input.get_axis("ui_left", "ui_right")
+
 	if _direction:
 		_last_direction = temp_direciton
 
 func _determine_has_jumped_or_stopped() -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		_has_jumped = is_on_floor() or _action == Action.JUMPING
+		_has_jumped = is_on_wall_only() or is_on_floor() or _action == Action.JUMPING
 	else:
 		_has_jumped = false
 	
@@ -67,14 +69,21 @@ func _determine_has_jumped_or_stopped() -> void:
 		
 func _determine_action() -> Action:
 	var on_floor = is_on_floor()
+	if is_on_wall_only() and _has_jumped:
+		return Action.WALL_JUMPING
 	if _has_jumped and _action == Action.JUMPING:
 		return Action.DOUBLE_JUMPING
 	if _has_jumped:
 		return Action.JUMPING
+	if is_on_wall_only():
+		return Action.WALL_SLIDING_DOWN if velocity.y > 0.0 else Action.WALL_SLIDING_UP
 	if not is_on_floor() and _is_jumping():
 		return _action
 	if not on_floor and not _is_jumping():
-		return Action.FALLING
+		if velocity.x > 0.0:
+			return Action.JUMPING
+		else:
+			return Action.FALLING
 	if on_floor and _has_direction():
 		return Action.WALKING
 	if on_floor and not _has_direction():
@@ -90,7 +99,12 @@ func _handle_lateral_movement(delta:float) -> void:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 
 func _handle_jump() -> void:
-	if _has_jumped:
+	if is_on_wall_only() and _has_jumped:
+			velocity.y = movement_config.JUMP_VELOCITY
+			_wall_jump_normal = get_wall_normal()
+			velocity.x = movement_config.SPEED * _wall_jump_normal.x
+			$WallJumpTimer.start()
+	elif _has_jumped:
 		if is_on_floor() or $CoyoteJumpTimer.time_left > 0.0:
 			velocity.y = movement_config.JUMP_VELOCITY
 			velocity += get_platform_velocity()
@@ -98,19 +112,7 @@ func _handle_jump() -> void:
 			velocity.y = movement_config.JUMP_VELOCITY * movement_config.DOUBLE_JUMP_SCALE
 	elif _has_stopped_jump:
 		velocity.y *= movement_config.SHORT_JUMP_SCALE
-		# if is_on_wall_only():
-		# 	velocity.y = player_movement_config.JUMP_VELOCITY
-		# 	wall_jump_normal = get_wall_normal()
-		# 	velocity.x = player_movement_config.SPEED * wall_jump_normal.x
-		# 	$WallJumpTimer.start()
-		# 	has_double_jumped = false
-		# elif is_on_floor() or $CoyoteJumpTimer.time_left > 0.0:
-		# 	velocity.y = player_movement_config.JUMP_VELOCITY
-		# 	velocity += get_platform_velocity()
-		# 	has_double_jumped = false
-		# elif not has_double_jumped:
-		# 	velocity.y = player_movement_config.JUMP_VELOCITY
-		# 	has_double_jumped = true
+		
 
 func _handle_coyote_timer(was_on_floor: bool) -> void:
 	if was_on_floor and not is_on_floor() and velocity.x >= 0.0:
@@ -118,10 +120,9 @@ func _handle_coyote_timer(was_on_floor: bool) -> void:
 
 func _apply_gravity(delta: float) -> void:
 	if not is_on_floor():
-		if is_on_wall_only() and velocity.y >= 0.0:
-			pass
-		# 	velocity += get_gravity() * player_movement_config.WALL_SLIDE_SCALE * delta
-		# 	velocity.y = min(velocity.y, player_movement_config.WALL_SLIDE_MAX_SPEED)
+		if is_on_wall_only() and velocity.y >= 0.0:	
+			velocity += get_gravity() * movement_config.WALL_SLIDE_SCALE * delta
+			velocity.y = min(velocity.y, movement_config.WALL_SLIDE_MAX_SPEED)
 		else:
 			velocity += get_gravity() * delta
 
@@ -134,8 +135,12 @@ func _animate_action() -> void:
 	match _action:
 		Action.WALKING:
 			$AnimatedSprite2D.play("walk")
-		Action.JUMPING, Action.DOUBLE_JUMPING:
+		Action.JUMPING, Action.DOUBLE_JUMPING, Action.FALLING:
 			$AnimatedSprite2D.play("jump")
+		Action.WALL_SLIDING_DOWN:
+			$AnimatedSprite2D.play("wall_slide_down")
+		Action.WALL_SLIDING_UP:
+			$AnimatedSprite2D.play("wall_slide_up")
 		_:
 			$AnimatedSprite2D.play("idle")
 
--- player_movement_config.gd
+++ player_movement_config.gd
@@ -3,13 +3,13 @@ extends Resource
 
 
 const SPEED = 300.0
-# const WALL_SLIDE_MAX_SPEED = 100.0
+const WALL_SLIDE_MAX_SPEED = 100.0
 const ACCELERATION = 1000.0
 const AIR_RESISTANCE = 300.0
 const JUMP_VELOCITY = -500.0
 const SHORT_JUMP_SCALE = 1.0/2.0
 const DOUBLE_JUMP_SCALE = 0.8
-# const WALL_SLIDE_SCALE = 1.0/6.0
+const WALL_SLIDE_SCALE = 1.0/6.0
 # const OFFSET_PER_INDEX = 0.1
 # const LERP_FRAME_COUNT = 15.0 #float to avoid constant float conversion
 # const OSCILLATION_WIDTH = 50.0
