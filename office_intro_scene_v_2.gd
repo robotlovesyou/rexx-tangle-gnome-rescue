5d2db25ce581be6a0b162f9ff015f08b8d4f160d
Andrew Smith <andy@robotlovesyou.com>
2025-11-03 16:44:54 +0100

    Implemented office scene v2

--- office_intro_scene_v_2.gd
+++ office_intro_scene_v_2.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
+@export var animation_player: AnimationPlayer
+@export var rexx: CutsceneRexx
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	animation_player.play("rexx_enters_stage_left")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
