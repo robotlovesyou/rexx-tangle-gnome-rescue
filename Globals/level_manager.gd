be8975b7e466ccb11b541b2b8fce4a6dec2825af
Andrew Smith <andy@robotlovesyou.com>
2025-11-13 17:48:40 +0100

    - Tweaked particles for spike trap - Moved player to be a PackedScene value on the level

--- Globals/level_manager.gd
+++ Globals/level_manager.gd
@@ -9,19 +9,19 @@ func replace_level_with(path: String) -> void:
 func set_active_spawn_point(spawn_point: SpawnPoint) -> void:
 	_spawn_point = spawn_point
 
-func spawn_player(root: Node, immediate_sibling: Node) -> Player:
+func spawn_player(player_scene: PackedScene, root: Node, immediate_sibling: Node) -> Player:
 	var candidates = get_tree().get_nodes_in_group("Player")
 	
 	if candidates.size() > 1:
 		# This can happen if the player is already spawning this tick
 		return
-	assert(candidates.size() == 1, "Cannot find player")
+	elif candidates.size() == 1:
+		var current_player = candidates[0] as Player
+		current_player.queue_free()
+		await current_player.tree_exited
 
-	var player = candidates[0] as Player
-	var path = player.scene_file_path
-	player.queue_free()
-	var scene = load(path)
-	player = scene.instantiate()
+	
+	var player = player_scene.instantiate()
 	root.add_child(player)
 	root.move_child(player, immediate_sibling.get_index()+1)
 	player.global_position = _spawn_point.global_position
--- Level-0-Training/training_level.gd
+++ Level-0-Training/training_level.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 @export var initial_dialog: InstructionDialog
+@export var player_scene: PackedScene
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	initial_dialog.expand()
-	Level.spawn_player(self, $Signs)
+	Level.spawn_player(player_scene, self, $Signs)
 	MovementHistory.reset($Player.position, Enums.Action.IDLING)
 	PMonitor.player = $Player
 	Events.player_hit_spike_trap.connect(_on_player_hit_spike_trap)
 
 func _on_player_hit_spike_trap(_trap: SpikeTrap) -> void:
-	Level.spawn_player(self, $Signs)
+	Level.spawn_player(player_scene, self, $Signs)
