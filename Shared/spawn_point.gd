0d30fe52c8fa2facda872ff9f5c36e7782eb0eb9
Andrew Smith <andy@robotlovesyou.com>
2026-02-14 16:37:53 +0100

    more level layout

--- Shared/spawn_point.gd
+++ Shared/spawn_point.gd
@@ -6,19 +6,51 @@ var _active := false
 @export var active: bool:
 	get: return _active
 	set(val): 
-		_active = val
-		if _active and Engine.is_editor_hint():
-			deactivate_other_spawn_points()
+		if val:
+			_activate_self()
+		else:
+			_deactivate_self()
+
+var _active_sprite: Sprite2D:
+	get: return $SpawnPointActive
+
+var _inactive_sprite: Sprite2D:
+	get: return $SpawnPointInactive
+
+var _active_particles: GPUParticles2D:
+	get: return $ActiveParticles
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if _active:
-		Level.spawn_point = self
+		_activate_self()
+	else:
+		_deactivate_self()
 
-func deactivate_other_spawn_points() -> void:
+func _deactivate_other_spawn_points() -> void:
 	if not is_inside_tree():
 		return
 		
 	for point in get_tree().get_nodes_in_group("SpawnPoint"):
 		if not point == self:
 			(point as SpawnPoint).active = false
+
+func _activate_self() -> void:
+	_active = true
+	_active_sprite.visible = true
+	_inactive_sprite.visible = false
+	_active_particles.emitting = true
+	_deactivate_other_spawn_points()
+	if !Engine.is_editor_hint():
+		Level.spawn_point = self
+
+func _deactivate_self() -> void:
+	_active = false
+	_active_sprite.visible = false
+	_inactive_sprite.visible = true
+	_active_particles.emitting = false
+
+
+func _on_activation_area_body_entered(body: Node2D) -> void:
+	if body is Player and !_active:
+		_activate_self()
